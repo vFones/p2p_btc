469e5f2ed32190c241668cf6fdeccd9b7dd958d6
Vittorio Fones <vittorio.fones@gmail.com>
2019-03-14 22:09:12 +0100

    Monitoring fifo with select

--- retibtc/src/n_node/node_handler.c
+++ retibtc/src/n_node/node_handler.c
@@ -33,6 +33,17 @@ static struct confirm_new_node choose_node()
   }
 }
 
+static void* fifo_handler()
+{
+  struct transaction trns;
+  printf("handling fifos-> ");
+  Read(fifo_fd, &trns, sizeof(trns));
+  printf("package from %s:%hu",trns.src, trns.srcport);
+  // TODO:
+  pthread_exit(NULL);
+}
+
+
 static void connect_to_network()
 {
   int node_n = 0, response = 0, i = 0;
@@ -56,22 +67,29 @@ static void connect_to_network()
     {
       int fd = Socket(AF_INET, SOCK_STREAM, 0);
       struct sockaddr_in tmp;
+      //socklen_t len = sizeof(struct sockaddr*);
       fillAddressIPv4(&tmp, new_conn.node->address, new_conn.node->port);
-      Connect(fd, (struct sockaddr *)&tmp);
-      sendInt(fd, NODE_CONNECTION);
+      visitConnectedNode(new_conn.node);
 
-      recvInt(fd, &response);
-      //if response is positive add to my list.
-      if(response)
+      if (connect(fd, (struct sockaddr *)&tmp, sizeof(struct sockaddr)) != -1)
       {
-        new_conn.node->fd = fd;
-        pthread_mutex_lock(&mtx_tree);
-          create_kid_to_node(connected_node, new_conn.node);
-        pthread_mutex_unlock(&mtx_tree);
-        succ_connection++;
+        sendInt(fd, NODE_CONNECTION);
+
+        recvInt(fd, &response);
+        //if response is positive add to my list.
+        if(response)
+        {
+          new_conn.node->fd = fd;
+          pthread_mutex_lock(&mtx_tree);
+            create_kid_to_node(connected_node, new_conn.node);
+          pthread_mutex_unlock(&mtx_tree);
+          succ_connection++;
+        }
+        else
+          printf("Node didn't accept, skip and go on\n");
       }
       else
-        printf("Node didn't accept, skip and go on\n");
+        perror("connect in node_handler:");
     }
     i++;
   }
@@ -110,7 +128,6 @@ static void* node_connection(void* arg)
   //TODO: download blockchain
   fprintf(stderr,"node_connection: pthread exit\n");
   pthread_exit(NULL);
-  return NULL;
 }
 
 static void close_connection()
@@ -163,16 +180,20 @@ void n_routine()
 {
   fprintf(stderr, "I'm [%d] forked from [%d]\n", getpid(), getppid());
   int optval = 1;
-  int list_fd = 0;
-  list_fd = Socket(AF_INET, SOCK_STREAM, 0);
-  setsockopt(list_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
-  //binding address on list_fd and setting queue
+  int list_fd = 0; // main FDs to monitor
+  struct sockaddr_in my_server_addr;
 
+  //mutex dinamically allcoated
   pthread_mutex_init(&mtx_tree, NULL);
 
-  struct sockaddr_in my_server_addr;
-  fillAddressIPv4(&my_server_addr, NULL, node_port);
+  //setting fd to monitor
+  fifo_fd = open(FIFOPATH, O_RDWR);
+  list_fd = Socket(AF_INET, SOCK_STREAM, 0);
 
+  // socket option with SO_REUSEADDR
+  setsockopt(list_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
+
+  // bindin address:port to socket fd and setting a backlog
   Bind(list_fd, (struct sockaddr*)&my_server_addr);
   Listen(list_fd, BACKLOG);
 
@@ -180,8 +201,8 @@ void n_routine()
 
   // used for select management and monitor
   int i_fd = 0, n_ready = 0;
-  fd_set fdset;
-  int fd = 0;
+  fd_set fdset; //fdset to fill in select while
+  int accept_fd = 0;
 
   int response = 0, request = 0;
 
@@ -194,43 +215,66 @@ void n_routine()
   int choice = 0;
   char line_buffer[16];
 
+
   //settin max_fd as list_fd and monitoring that on fd_open table
   fd_open = (int *)calloc(FD_SETSIZE, sizeof(int));
-  max_fd = list_fd;
+
+  fd_open[fifo_fd] = 1;
+  printf("Fifo_Fd %d", fifo_fd);
+  fd_open[list_fd] = 1;
+  printf("List_Fd %d", list_fd);
+
+  printf("Prima Max_Fd %d\n", max_fd);
+  max_fd = (fifo_fd > list_fd) ? fifo_fd : list_fd;
+  printf("Dopo Max_Fd %d\n", max_fd);
   fd_open[max_fd] = 1;
+  // setting list_fd as max since fifo_fd is created before
 
   while (1)
   {
     FD_ZERO(&fdset);
     FD_SET(STDIN_FILENO, &fdset);
+    FD_SET(fifo_fd, &fdset);
     FD_SET(list_fd, &fdset);
     //re update fdset with fd_open monitor table
     for (i_fd = 0; i_fd <= max_fd; i_fd++)
       if (fd_open[i_fd])
         FD_SET(i_fd, &fdset);
 
+    printf("\tChoose:\n1) connect to peers;\n2) disconnect from peer\n5) quit\n");
+    printf("Connected node:\n");
+    visit_tree(connected_node, visitConnectedNode);
+
     while ( (n_ready = select(max_fd + 1, &fdset, NULL, NULL, NULL)) < 0 );
     if (n_ready < 0 )
-    { /* on real error exit */
+    {
       perror("select error");
       exit(1);
     }
 
-    /*******************
+    /*********************
         STDIN menu_case
-    *******************/
+    **********************/
     if(FD_ISSET(STDIN_FILENO, &fdset))
     {
       n_ready--;
-      printf("\tChoose:\n1) connect to peers;\n2) disconnect from peer\n5) quit\n");
-      printf("Connected node:\n");
-      visit_tree(connected_node, visitConnectedNode);
-
       fflush(stdin);
       fgets(line_buffer, 16, stdin);
       choice = atoi(line_buffer);
       menu_case(choice);
     }
+
+    /***********************
+        FIFO comunication
+    ************************/
+    if(FD_ISSET(fifo_fd, &fdset))
+    {
+      n_ready--;
+      printf("received transaction from %s\n", FIFOPATH);
+      pthread_create(&tid[tid_index], NULL, fifo_handler, NULL);
+      tid_index++;
+    }
+
 
     /***************************
         Socket connections
@@ -239,19 +283,19 @@ void n_routine()
     {
       n_ready--; //accepting after new connection
       int keepalive = 1;
-      if ((fd = accept(list_fd, NULL, NULL)) < 0)
+      if ((accept_fd = accept(list_fd, NULL, NULL)) < 0)
       {
         perror("accept");
         exit(1);
       }
-      setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &keepalive, sizeof(keepalive));
+      setsockopt(accept_fd, SOL_SOCKET, SO_KEEPALIVE, &keepalive, sizeof(keepalive));
 
       // setting fd ready for reading
-      fd_open[fd] = 1;
+      fd_open[accept_fd] = 1;
 
       //calculating new max
-      if (max_fd < fd)
-        max_fd = fd;
+      if (max_fd < accept_fd)
+        max_fd = accept_fd;
     }
     i_fd = list_fd;
 
@@ -290,12 +334,12 @@ void n_routine()
           continue;
         }
 
-        tid_args[tid_index%BACKLOG] = i_fd;
+        tid_args[tid_index] = i_fd;
         //request received correctly, switching between cases
         switch(request)
         {
           case NODE_CONNECTION:
-            pthread_create(&tid[tid_index%BACKLOG], NULL, node_connection, (void *)&tid_args[tid_index%BACKLOG]);
+            pthread_create(&tid[tid_index], NULL, node_connection, (void *)&tid_args[tid_index]);
             break;
           /*
           case WALLET_CONNECTION:
