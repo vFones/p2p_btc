c9fbf966ff490d2e71e8483641b2b1ed51b8a80e
Vittorio Fones <vittorio.fones@gmail.com>
2019-03-16 16:34:20 +0100

    Created blockchains main functions for project.

--- retibtc/include/blockchain.h
+++ retibtc/include/blockchain.h
@@ -1,15 +1,35 @@
 #ifndef BLOCKCHAIN_H
 #define BLOCKCHAIN_H
 
-#include "net.h"
-#include "transaction.h"
+#include "tree.h"
+
 
 struct block{
+  char *prev_sha1;
+  char *sha1;
   int n_block;
   int randomtime;
-  struct transaction trnsn;
+  void *info;
 };
 typedef struct block* Block;
 #define BLOCK_SIZE sizeof(struct block)
+
+struct blockchain{
+  Tree genesis;
+  Tree tail;
+  int b_size;
+};
+typedef struct blockchain* Blockchain;
+#define BCHAIN_SIZE sizeof(struct blockchain)
+
+
+Blockchain create_blockchain();
+
+//handling multitail
+void addBlockToBlockchain(Blockchain blockchain, struct block block);
+
+struct block getBlockFromNode(Tree node);
+
+struct block searchByLevel(Blockchain blockchain, int level);
 
 #endif
--- retibtc/lib/blockchain.c
+++ retibtc/lib/blockchain.c
@@ -1,1 +1,107 @@
 #include "../include/blockchain.h"
+
+Blockchain create_blockchain()
+{
+  struct block genesis;
+  genesis.sha1 = "GENESIS";
+  genesis.prev_sha1 = NULL;
+  genesis.n_block = 0;
+  genesis.randomtime = 0;
+  genesis.info = NULL;
+
+  Blockchain blockchain = (Blockchain)Malloc(BCHAIN_SIZE);
+
+  blockchain->genesis = (Tree)Malloc(TREE_SIZE);
+  blockchain->tail = blockchain->genesis;
+  blockchain->genesis->info = (void *)&genesis;
+
+  return blockchain;
+}
+
+
+//used to get block from a specific node
+struct block getBlockFromNode(Tree node)
+{
+  struct block b = *(struct block *) node->info;
+  return b;
+}
+
+
+static Tree max_randtime(Tree node)
+{
+  int tmpmax=0;
+  struct block n_info, sibl_info;
+
+  Tree found = NULL, tmp = NULL;
+  found = node;
+  tmp = node;
+
+  while(tmp->siblings != NULL)
+  {
+    n_info = getBlockFromNode(tmp);
+    sibl_info = getBlockFromNode(tmp->siblings);
+    // A > B
+    if(n_info.randomtime > sibl_info.randomtime)
+    {
+      if(n_info.randomtime > tmpmax)// A > FOUND
+      {
+        tmpmax = n_info.randomtime;
+        found = tmp;
+      }
+    }
+    else // B > A
+    {
+      if(sibl_info.randomtime > tmpmax)// B > FOUND
+      {
+        tmpmax = n_info.randomtime;
+        found = tmp->siblings;
+      }
+    }
+    tmp = tmp->siblings; // SHIFT A - B to B - C
+  } // utill D = NULL
+  return found; // return greatest
+}
+
+
+// add a new block to blockchain checking for multitail
+void addBlockToBlockchain(Blockchain blockchain, struct block block)
+{
+  Tree tmp = blockchain->tail;
+  Tree multitail = NULL;
+  Tree new_son = NULL;
+  // if latest node of blockchain got siblings
+  // use max_randtime to checkup the brother with max rand time
+  // and then add block to him son;
+
+  if(!has_node_siblings(tmp))
+  {
+    new_son = create_kid_to_node(tmp, &block);
+    blockchain->tail = new_son;
+  }
+  else// there are multitail
+  {
+    multitail = max_randtime(tmp);
+    new_son = create_kid_to_node(multitail, &block);
+    blockchain->tail = new_son;
+  }
+  blockchain->b_size++;
+}
+
+
+// return block with that level
+struct block searchByLevel(Blockchain blockchain, int level) {
+  Tree tmp = blockchain->genesis->kids;
+  struct block b;
+  int i = 0;
+
+  while (tmp->kids != NULL || i < level)
+  {
+    tmp = tmp->kids;
+    i++;
+  }
+  if (has_node_siblings(tmp))
+    tmp = max_randtime(tmp);
+
+  b = *(struct block *)(tmp->info);
+  return b;
+}
